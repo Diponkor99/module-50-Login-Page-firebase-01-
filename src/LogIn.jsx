ecab7bbd38efaa23fcefc78b4c4d50e7c6b097b9
Diponkor <roydiponker891@gmail.com>
2025-08-30 23:40:30 +0600

    firebase login , email varification

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,28 +1,58 @@
-import React from 'react';
+import { signInWithEmailAndPassword,} from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from './firebase.init';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
 
 const LogIn = () => {
-    const handelOnSubmit=(event)=>{
+    const [errormessage, setErrormessage]=useState('')
+    const [SuccessMessage , setSuccessmessage]=useState(false);
+    const[showPassword , setShowPassword]=useState(false);
+    const handelOnSubmit = (event) => {
         event.preventDefault();
-      const Email=event.target.email.value;
-      const Password=event.target.password.value;
-      console.log("Email :",Email,
-                  "Password :",Password)
+        const Email = event.target.email.value;
+        const Password = event.target.password.value;
+        console.log("Email :", Email,"Password :", Password)
+        setErrormessage('')
+        setSuccessmessage(false)
+        //User login...
+        signInWithEmailAndPassword(auth , Email, Password)
+        .then(result=>{
+            console.log(result)
+            setSuccessmessage(true)
+
+        })
+        .catch(error=>{
+            console.log(error.message)
+            setErrormessage(error.message)
+        })
     }
     return (
+       
         <div className=' w-11/12  h-screen flex items-center mx-auto'>
-            
 
+          
             <section className='w-110 rounded-2xl mx-auto p-9 bg-lime-300 shadow shadow-2xl shadow-amber-500'>
                 <h1 className='font-bold text-2xl mb-5'>LogIn page :</h1>
-                <form onSubmit={handelOnSubmit}>
+                <form onSubmit={handelOnSubmit} className='relative'>
                     <h1 className='p-1'>Email:</h1>
-                   <input type="email" name='email' placeholder="Please your Email:" className="input w-full" />
-                   <h1 className='pt-3'>Password:</h1>
-                   <input type="password" name="password" placeholder="Please your Password:" className="input w-full" />
-                   <tr></tr>
-                   <button className='btn my-3 w-full btn-success'>Submit</button>
+                    <input type="email" name='email' placeholder="Please your Email:" className="input w-full" />
+                    <h1 className='pt-3'>Password:</h1>
+                    <input type={showPassword ? "text":"password"} name="password" placeholder="Please your Password:" className="input w-full" />
+                    {/* showPassword react Icone */}
+                    <h1 onClick={()=>(setShowPassword(!showPassword))} className='absolute right-4 top-30'>{showPassword ? <FaEyeSlash></FaEyeSlash>:<FaEye></FaEye>}</h1>
+                    <a className='flex justify-end hover:underline' href="">Forget Password?</a>
+                    <tr></tr>
+                    <button className='btn my-3 w-full btn-success'>Submit</button>
                 </form>
-
+                {
+                    errormessage && <p className=' text-red-700'>{errormessage}</p>
+                }
+                {
+                    SuccessMessage && <p className='text-green-800'>Success your login</p>
+                }
+            <h1 className='font-medium text-xl text-amber-900'>Please create an account! <Link className='hover:underline text-xl font-bold' to='/reg'>Sign Up</Link></h1>
             </section>
         </div>
     );
--- src/Registation.jsx
+++ src/Registation.jsx
@@ -4,92 +4,126 @@ import { Link } from 'react-router-dom';
 import { auth } from './firebase.init';
 import { Bounce, toast, ToastContainer } from 'react-toastify';
 import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { sendEmailVerification } from 'firebase/auth';
+
 
 const Registation = () => {
 
-  const [errormasses , setErrormasses]=useState("")
-  const handelonSubmit=(event)=>{
+  const [errormasses, setErrormasses] = useState("");
+  const [chackPassword, setChackpassword] = useState(false);
+  const handelonSubmit = (event) => {
     event.preventDefault()
     setErrormasses('')
-    
-      const FirstName=event.target.firstN.value;
-      const LastName=event.target.LastN.value;
-      const Email=event.target.email.value;
-      const Password=event.target.password.value;
-      console.log(FirstName, LastName, Email, Password)
-      //password valitation ....
-      if(Password.length < 5){
-        setErrormasses('Please add at least 6 characters in password')
-          return;}
 
-        const UpperCharacter=/[A-Z]/;        //Uppercase Character cheack??
-      if (!UpperCharacter.test(Password)) {
-        setErrormasses('Please add at least uppercase characters in password')
-        return;
-      } 
-      
-      const lowerCase=/[a-z]/;     //lowerCase character cheack???
-      if(!lowerCase.test(Password)){
-        setErrormasses('Please add at least lowerCase characters in password')
-        return;
-      }
+    const FirstName = event.target.firstN.value;
+    const LastName = event.target.LastN.value;
+    const Email = event.target.email.value;
+    const Password = event.target.password.value;
+    const ChackBox=event.target.chackbox.checked;
+    console.log(FirstName, LastName, Email, Password ,ChackBox)
+    //password valitation ....
+    if(!ChackBox){
+      setErrormasses('Please Checkbox Active')
+      return;
+    }
+    if (Password.length < 5) {
+      setErrormasses('Please add at least 6 characters in password')
+      return;
+    }
 
-      const spacalCharacter=/[!@#$%&?]/;    // least one special character in password
-         if(!spacalCharacter.test(Password)){
-          setErrormasses('Please add at least special character in password')
-          return;
-         }
-      
+    const UpperCharacter = /[A-Z]/;        //Uppercase Character cheack??
+    if (!UpperCharacter.test(Password)) {
+      setErrormasses('Please add at least uppercase characters in password')
+      return;
+    }
 
-      //firebase add email & password.....
-      createUserWithEmailAndPassword(auth,Email, Password)
-      .then(result=>{
+    const lowerCase = /[a-z]/;     //lowerCase character cheack???
+    if (!lowerCase.test(Password)) {
+      setErrormasses('Please add at least lowerCase characters in password')
+      return;
+    }
+
+    const spacalCharacter = /[!@#$%&?]/;    // least one special character in password
+    if (!spacalCharacter.test(Password)) {
+      setErrormasses('Please add at least special character in password')
+      return;
+    }
+
+
+    //firebase add email & password.....
+    createUserWithEmailAndPassword(auth, Email, Password)
+      .then(result => {
         console.log(result);
         toast.success("Registration Successful 🎉");
+        //Email varification sent .......
+       sendEmailVerification(result.user)
+                .then(()=>{
+                   toast("Verification email sent!");
+                })
+
       })
-      .catch(error=>{
+      .catch(error => {
         console.log(error);
         setErrormasses(error.message);
       })
 
   }
-    return (
-        <div>
-          <ToastContainer position="top-right" autoClose={5000} Teanstion={Bounce} ></ToastContainer>
-            <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Registration Now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-       <form onSubmit={handelonSubmit}>
-      <fieldset className="fieldset">
-        <label className="label">First Name</label>
-          <input type="text" name='firstN' className="input" placeholder="First Name" />
-          <label className="label">Last Name</label>
-          <input type="text" name='LastN' className="input" placeholder="Last Name" />
+  return (
+    <div>
+      <ToastContainer position="top-right" autoClose={5000} Teanstion={Bounce} ></ToastContainer>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Registration Now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body ">
+              <form onSubmit={handelonSubmit}>
+                <fieldset className="fieldset relative">
+                  <label className="label">First Name</label>
+                  <input type="text" name='firstN' className="input" placeholder="First Name" />
+                  <label className="label">Last Name</label>
+                  <input type="text" name='LastN' className="input" placeholder="Last Name" />
 
-          <label className="label">Email</label>
-          <input type="email" name='email' className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" name='password' className="input" placeholder="Password" />
-         
-          <button className="btn btn-neutral mt-4">Register Now</button>
-        </fieldset>
-       </form>
-{errormasses && <p className="text-red-600 font-semibold">{errormasses}</p>}
-       <p className='flex justify-end'>Already Register.<Link to="/login" className='text-red-800 hover:underline'>Please logIn</Link></p>
+                  <label className="label">Email</label>
+                  <input type="email" name='email' className="input" placeholder="Email" />
+                  <label className="label">Password</label>
+                  <input type={chackPassword ? "text" : "password"} name='password' className="input" placeholder="Password" />
+                <h1
+                  onClick={() => (setChackpassword(!chackPassword))}
+                className=' text-xl absolute right-9 bottom-19'>
+                {chackPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye >}
+                </h1>
+                 
+                 <button className="btn btn-neutral mt-4">Register Now</button>
+
+                </fieldset>
+                
+                 <div className='flex gap-2 mt-2'>
+                <input
+                type="checkbox"
+                className="checkbox "
+                name='chackbox'
+              />
+              <h1 className='font-bold'>Accept Privacy Policy</h1>
+              </div>
+              </form>
+
+              {errormasses && <p className="text-red-600 font-semibold">{errormasses}</p>}
+
+              
+              <p className='flex justify-end'>Already Register.<Link to="/login" className='text-red-800 hover:underline'>Please logIn</Link></p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Registation;
